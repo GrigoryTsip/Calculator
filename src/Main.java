5ffbc0308095bfcf58cc7561031f0441c1b5eab4
Grigory Tsiperman <113826725+GrigoryTsip@users.noreply.github.com>
2022-10-29 06:02:17 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Calculator calc = Calculator.instance.get();;
+
+        int a = calc.plus.apply(8, 2);
+        int b = calc.minus.apply(1,3);
+        calc.println.accept("a = " + a);
+        calc.println.accept("b = " + b);
+
+        b = calc.abs.apply(b);
+        calc.println.accept("|b| = " + b);
+
+        int c = calc.devide.apply(a, b);
+        calc.println.accept("c = a/|b| = " + c);
+
+        c = calc.pow.apply(c);
+        calc.println.accept("c^2 = " + c);
+
+        c = calc.multiply.apply(c,b);
+        calc.println.accept("c * |b| = " + c);
+
     }
 }
